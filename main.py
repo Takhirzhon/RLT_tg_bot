8043154947091c6fc9a198bd0c21004a6119f027
Tokhirzhon Tashmatov <tashmatovtahir@gmail.com>
2025-12-16 20:22:24 +0600

    Fix DB issue

--- main.py
+++ main.py
@@ -77,4 +77,6 @@ async def main() -> None:
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    if os.name == 'nt':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
